5ca6e69601d0901bd14cd8eb9aa18ec071f82af6
TomaszKrzeminski <koral2323@gmail.com>
2023-01-04 10:13:54 +0100

    First Commit

---
+++ HotelWpfApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HotelWpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
